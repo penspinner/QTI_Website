a8aa7827c8d88fbef5bbaaaad16186bf49403aae
stevenliao <Stvnliao@yahoo.com>
2017-02-28 00:34:32 -0500

    Created new files for page tabs

--- js/main.js
+++ js/main.js
@@ -9,37 +9,128 @@ angular.element(document).ready(function()
 				$urlRouterProvider.otherwise('/');
 				$locationProvider.html5Mode(true);
 				$stateProvider
-					.state('home',
-					{
-						url: '/',
-						templateUrl: 'partials/home.html',
-						controller: 'HomeController'
-					})
-					.state('about',
-					{
-						url: '/about',
-						templateUrl: 'partials/about.html'
-					})
-					.state('services',
-					{
-						url: '/services',
-						templateUrl: 'partials/services.html'
-					})
-					.state('clients',
-					{
-						url: '/clients',
-						templateUrl: 'partials/clients.html'
-					})
-					.state('partners',
-					{
-						url: '/partners',
-						templateUrl: 'partials/partners.html'
-					})
-					.state('testimonials',
-					{
-						url: '/testimonials',
-						templateUrl: 'partials/testimonials.html'
-					});
+                    .state('home',
+                    {
+                        url: '/',
+                        templateUrl: 'partials/home.html',
+                        controller: 'HomeController'
+                    })
+                    .state('about',
+                    {
+//                        abstract: true,
+                        url: '/about',
+                        templateUrl: 'partials/about.html',
+                        controller: 
+                        [
+                            '$scope', function($scope)
+                            {
+                                $scope.aboutTabs = 
+                                [
+                                    {
+                                        name: 'Cloud Services',
+                                        url: '.cloudservices'
+                                    },
+                                    {
+                                        name: 'Managed Services',
+                                        url: '.managedservices'
+                                    },
+                                    {
+                                        name: 'Recruitment',
+                                        url: '.recruitment'
+                                    },
+                                    {
+                                        name: 'Our Company',
+                                        url: '.ourcompany'
+                                    },
+                                    {
+                                        name: 'Procurement',
+                                        url: '.procurement'
+                                    }
+                                ];
+                            }
+                        ]
+                    })
+                    .state('about.cloudservices',
+                    {
+                        url: '/cloudservices',
+                        templateUrl: 'partials/about.cloudservices.html'
+                    })
+                    .state('about.managedservices',
+                    {
+                        url: '/managedservices',
+                        templateUrl: 'partials/about.managedservices.html'
+                    })
+                    .state('about.recruitment',
+                    {
+                        url: '/recruitment',
+                        templateUrl: 'partials/about.recruitment.html'
+                    })
+                    .state('about.ourcompany',
+                    {
+                        url: '/ourcompany',
+                        templateUrl: 'partials/about.ourcompany.html'
+                    })
+                    .state('about.procurement',
+                    {
+                        url: '/procurement',
+                        templateUrl: 'partials/about.procurement.html'
+                    })
+                    .state('services',
+                    {
+                        url: '/services',
+                        templateUrl: 'partials/services.html'
+                    })
+                    .state('clients',
+                    {
+                        url: '/clients',
+                        templateUrl: 'partials/clients.html'
+                    })
+                    .state('resources',
+                    {
+                        url: '/resources',
+                        templateUrl: 'partials/resources.html',
+                        controller: 
+                        [
+                            '$scope', function($scope)
+                            {
+                                $scope.resourcesTabs = 
+                                [
+                                    {
+                                        name: 'Partners',
+                                        url: '.partners'
+                                    },
+                                    {
+                                        name: 'Documents',
+                                        url: '.documents'
+                                    },
+                                    {
+                                        name: 'Testimonials',
+                                        url: '.testimonials'
+                                    }
+                                ];
+                            }
+                        ]
+                    })
+                    .state('resources.partners',
+                    {
+                        url: '/partners',
+                        templateUrl: 'partials/resources.partners.html'
+                    })
+                    .state('resources.documents',
+                    {
+                        url: '/documents',
+                        templateUrl: 'partials/resources.documents.html'
+                    })
+                    .state('resources.testimonials',
+                    {
+                        url: '/testimonials',
+                        templateUrl: 'partials/resources.testimonials.html',
+                    })
+                    .state('testimonials',
+                    {
+                        url: '/testimonials',
+                        templateUrl: 'partials/testimonials.html'
+                    });
 			}]);
 
 	var body = document.getElementsByTagName("body")[0];
